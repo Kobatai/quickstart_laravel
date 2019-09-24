73a2cb0ab6736095c83a79a6e1d326739644104c
Kobatai <taiga1983346@gmail.com>
2019-09-24 21:25:49 +0900

    create POST method

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ use Illuminate\Http\Request;
 //タスク一覧表示
 Route::get('/',function(){
 
+  $tasks = Task::orderBy('created_at','asc')->get();
+  return view('tasks',[
+    'tasks' => $tasks
+  ]);
 });
 
 //タスク追加
@@ -24,10 +28,28 @@ Route::post('/task',function(Request $request){
   //Request $requestでPOSTの値を取得できる
   // use Illuminate\Http\Request;を書かないとエラーになる
 
-})
+  $validator = Validator::make($request->all(),[
+    'name'=>'required|max:255',
+  ]);
+
+  if($validator->fails()){
+    //リダイレクト先までInputとErrorのデータをフラッシュデータとしてセッションに保存して持ち越す
+    //errorsはcommon.errorsで表示
+    return redirect('/')
+    ->withInput()
+    ->withErrors($validator);
+  }
+
+  $task = new Task;
+  $task->name = $request->name;
+  $task->save();
+
+  return redirect('/');
+
+});
 
 //タスク削除
 Route::delete('task/{task}',function(Task $task){
   //{task}という記述で消すtaskを特定し、Task $taskにパラメーターを送る
   //use App\Task;がないとエラーになる
-})
+});
