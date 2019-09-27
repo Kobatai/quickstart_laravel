72e1cf5e3be1676ffb73bbdc277b62463ed5eca1
Kobatai <taiga1983346@gmail.com>
2019-09-27 13:44:53 +0900

    finish

--- routes/web.php
+++ routes/web.php
@@ -15,41 +15,48 @@ use App\Task;
 use Illuminate\Http\Request;
 
 //タスク一覧表示
-Route::get('/',function(){
+// Route::get('/',function(){
 
-  $tasks = Task::orderBy('created_at','asc')->get();
-  return view('tasks',[
-    'tasks' => $tasks
-  ]);
-});
+//   $tasks = Task::orderBy('created_at','asc')->get();
+//   return view('tasks',[
+//     'tasks' => $tasks
+//   ]);
+// });
 
-//タスク追加
-Route::post('/task',function(Request $request){
-  //Request $requestでPOSTの値を取得できる
-  // use Illuminate\Http\Request;を書かないとエラーになる
 
-  $validator = Validator::make($request->all(),[
-    'name'=>'required|max:255',
-  ]);
+// //タスク追加
+// Route::post('/task',function(Request $request){
+//   //Request $requestでPOSTの値を取得できる
+//   // use Illuminate\Http\Request;を書かないとエラーになる
 
-  if($validator->fails()){
-    //リダイレクト先までInputとErrorのデータをフラッシュデータとしてセッションに保存して持ち越す
-    //errorsはcommon.errorsで表示
-    return redirect('/')
-    ->withInput()
-    ->withErrors($validator);
-  }
+//   $validator = Validator::make($request->all(),[
+//     'name'=>'required|max:255',
+//   ]);
 
-  $task = new Task;
-  $task->name = $request->name;
-  $task->save();
+//   if($validator->fails()){
+//     //リダイレクト先までInputとErrorのデータをフラッシュデータとしてセッションに保存して持ち越す
+//     //errorsはcommon.errorsで表示
+//     return redirect('/')
+//     ->withInput()
+//     ->withErrors($validator);
+//   }
 
-  return redirect('/');
+//   $task = new Task;
+//   $task->name = $request->name;
+//   $task->save();
 
-});
+//   return redirect('/');
+
+// });
 
 //タスク削除
-Route::delete('task/{task}',function(Task $task){
-  //{task}という記述で消すtaskを特定し、Task $taskにパラメーターを送る
-  //use App\Task;がないとエラーになる
-});
+// Route::delete('task/{task}',function(Task $task){
+//   //{task}という記述で消すtaskを特定し、Task $taskにパラメーターを送る
+//   //use App\Task;がないとエラーになる
+// });
+
+Route::get('/tasks','TasKController@index');
+
+Route::post('/task','TaskController@store');
+
+Route::delete('/task/{taks}','TaskController@destroy');
