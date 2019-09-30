353c891d480626f642835abcc45e14e5d59daf01
Kobatai <taiga1983346@gmail.com>
2019-09-30 16:37:41 +0900

    create Controller

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,5 +6,44 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    //新タスク作成
+    //@param Request @request
+    //@return Response
+
+    public function store(Request $request){
+    	$this->validate($request,[
+    		'name' => 'required|max:255',
+    	]);
+
+    	$task = new Task;
+    	$task->name = $request->name;
+    	$task->save();
+
+    	return redirect('/tasks');
+    }
+
+    //タスクの表示
+    //@param Request @request
+    //@return Response
+
+   public function index(Request $request){
+
+   	  $tasks = Task::orderBy('created_at','asc')->get();
+
+   	  return view('tasks',[
+   	  	'tasks'=>$tasks
+   	  ]);
+   }
+
+	// 指定タスクの削除
+	//@param Request @request
+    //@param Task $task
+	//@return Response
+
+   public function destroy(Request $request, Task $task){
+
+   	 $task->delete();
+
+   	 return view('/tasks');
+   }
 }
